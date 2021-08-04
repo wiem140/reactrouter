56a72cfb690713828a95452f35d85264b074ae0b
wiem140 <84313071+wiem140@users.noreply.github.com>
2021-08-04 11:36:52 +0100

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+
+export const movieData =[
+    {
+        id: Math.random(),
+        name:"Titanic",
+        date:"1997",
+        image:"https://media.senscritique.com/media/000019629037/source_big/Titanic.jpg",
+        description:"Titanic est un film dramatique américain écrit, produit et réalisé par James Cameron, sorti en 1997.intégrant à la fois des aspects historiques et fictifs, le film est basé sur le récit du naufrage du RMS Titanic et met en vedette Leonardo DiCaprio et Kate Winslet",
+        type:"film dramatique",
+        rating:5
+    },
+    {
+        id: Math.random(),
+        name:" Happy New Year",
+        date:"2014",
+        image:"https://i.pinimg.com/originals/37/41/33/374133718551dfcfa4c58819e61f4835.jpg",
+        description:"est un film indien réalisé par Farah Khan, sorti en 2014.les rôles principaux sont tenus par Shahrukh Khan, Abhishek Bachchan et Deepika Padukone. C'est un projet que la réalisatrice et chorégraphe indienne porte depuis 2005 et dont le tournage a été de nombreuses fois reporté.",
+        type:"Romantique",
+        rating:4
+    },
+{
+    id: Math.random(),
+        name:"365 Days of Happiness",
+        date:"2011",
+        image:"https://www.courrierinternational.com/sites/ci_master/files/styles/image_original_765/public/assets/images/illustrations/article/2011/02/1057/365days-affiche.jpg?itok=ICa1sqq.",
+        description:"Hady has it good. He's a handsome millionaire playboy, women fall at his feet, and he loves and leaves them quilt-free. In Hady's mind his life is perfect but then fate deals him a hand he isn't expecting... the woman of his dreams.",
+        type:"film romantique",
+        rating:5}
+
+]
